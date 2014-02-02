d9287864392440d9fdd9b176a637ef6c715128c5
Stephen Kuenzli <skuenzli@qualimente.com>
2014-02-02 12:27:35 -0700

    create simple script to perform canonical command-line build and test for library.

---
+++ build_and_test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo "starting build and test of WeblogNG Javascript client"
+grunt clean development karma:continuous
+grunt_exit_status=$?
+echo "grunt exit status: ${grunt_exit_status}"
+echo "finished build and test of WeblogNG Javascript client"
+exit ${grunt_exit_status}
