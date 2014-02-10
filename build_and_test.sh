e953895e6c0daa2e3de7ea6a717eca58108af6f4
Stephen Kuenzli <skuenzli@qualimente.com>
2014-02-09 18:08:44 -0700

    Update script to call default instead of development to align with updated Gruntfile.js.

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "starting build and test of WeblogNG Javascript client"
-grunt clean development karma:continuous
+grunt clean default karma:continuous
 grunt_exit_status=$?
 echo "grunt exit status: ${grunt_exit_status}"
 echo "finished build and test of WeblogNG Javascript client"
