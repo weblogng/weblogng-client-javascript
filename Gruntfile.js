8320b39932856d1b1ffdcc0e670ba05f3d1860c8
Stephen Kuenzli <skuenzli@weblogng.com>
2015-02-07 14:37:05 -0700

    Integrate grunt-release plugin.

--- Gruntfile.js
+++ Gruntfile.js
@@ -112,10 +112,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-release');
 
   // Default task.
   grunt.registerTask('default', [
